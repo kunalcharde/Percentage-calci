053db952dd85b819bcfebb800cf4a03ca5e22eba
Kunal Charde <kunalcharde1@gmail.com>
2023-01-28 13:21:16 +0530

    Updated

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const id2 = document.getElementById("normal-percentage")
 const id3 = document.getElementById("semester-percentage")
 const back_btn1 = document.querySelectorAll('.back-btn')[0]
 const back_btn2 = document.querySelectorAll('.back-btn')[1]
-
+const obt_mark = document.getElementById("obt-marks");
+const total_mark = document.getElementById("total-marks");
 // function to set the hide and visibility
 function hide(id1, id2) {
     id1.classList.add("hide");
@@ -35,6 +36,8 @@ back_btn1.addEventListener("click", (e) => {
     e.preventDefault();
     id1.classList.remove("hide");
     hide(id2, id3)
+    obt_mark.value = ""
+    total_mark.value = ""
 })
 // Back Button of Multiple Subject/Semesters Secetion UI
 back_btn2.addEventListener("click", (e) => {
@@ -49,12 +52,11 @@ back_btn2.addEventListener("click", (e) => {
     document.getElementById("sem-num").innerText = "Enter 1st Semesters Marks";
     document.getElementById("sem-calculate-btn").setAttribute('disabled', '')
     document.getElementById("sem-display-percentage").innerText = ""
+    document.getElementById('preview').innerText =""
     console.log(document.getElementById("input-semesters").value)
 })
 
-
 // CGPA Calculation BTN
-
 const cgpa_btn = document.getElementById('cgpa-btn')
 cgpa_btn.addEventListener("click", () => {
     alert("This Service Comeing Soon")
@@ -64,11 +66,15 @@ cgpa_btn.addEventListener("click", () => {
 // Calculate Button of Normal Secetion UI
 const Calculate_btn = document.getElementById("calculate-btn")
 Calculate_btn.addEventListener("click", () => {
-    const obt_mark = document.getElementById("obt-marks").value;
-    const total_mark = document.getElementById("total-marks").value;
-    let percentage = (obt_mark / total_mark) * 100;
-    console.log(percentage)
-    document.getElementById("normal-display-percentage").innerText = `Your Percentage calculation is ${percentage} %`
+
+    if (parseInt(total_mark.value) >= parseInt(obt_mark.value)) {
+        console.log("Total", total_mark.value)
+        let percentage = ((obt_mark.value / total_mark.value) * 100).toFixed(2);
+        document.getElementById("normal-display-percentage").innerText = `Your Percentage calculation is ${percentage} %`
+    }
+    else {
+        alert("Obtained Marks Should Less then or equal to Total Marks")
+    }
 })
 
 // Enter Button of Multiple Subject/Semesters Secetion UI
@@ -79,28 +85,40 @@ let total_mark_arr = [];
 let count = 1;
 enter_btn.addEventListener("click", (e) => {
     e.preventDefault();
-    const input_semesters = document.getElementById("input-semesters").value;
+    const input_semesters = document.getElementById("input-semesters");
     const sem_obt_mark = document.getElementById("sem-obt-marks");
     const sem_total_mark = document.getElementById("sem-total-marks");
-    if (sem_obt_mark.value != "" && sem_total_mark.value != "" && input_semesters.value != "") {
-        if (obt_mark_arr.length < input_semesters && total_mark_arr < input_semesters) {
-            ++count;
-            obt_mark_arr.push(parseInt(sem_obt_mark.value));
-            total_mark_arr.push(parseInt(sem_total_mark.value));
-            document.getElementById("sem-num").innerText = `Enter ${count} Semesters Marks`
-            sem_obt_mark.value = "";
-            sem_total_mark.value = "";
+    //validation
+    if(input_semesters.value===""){
+        alert("Please Enter Number of Semesters / Subject")
+        return
+    }
+    if (parseInt(sem_total_mark.value) >= parseInt(sem_obt_mark.value)) {
+        if (sem_obt_mark.value != "" && sem_total_mark.value != "" && input_semesters.value != "") {
+            if (obt_mark_arr.length < input_semesters.value && total_mark_arr < input_semesters.value) {
+                ++count;
+                obt_mark_arr.push(parseInt(sem_obt_mark.value));
+                total_mark_arr.push(parseInt(sem_total_mark.value));
+                document.getElementById("sem-num").innerText = `Enter ${count} Semesters Marks`
+                document.getElementById('preview').innerText = `You Entered Obtained Marks: ${sem_obt_mark.value} and Total Marks : ${sem_total_mark.value} of ${count - 1} Semester/subject`
+                sem_obt_mark.value = "";
+                sem_total_mark.value = "";
+            }
+            else {
+                document.getElementById("sem-num").innerText = `Successfull Entered ${count} Semester Marks`
+                document.getElementById("sem-calculate-btn").removeAttribute("disabled")
+                return
+            }
         }
         else {
-            document.getElementById("sem-num").innerText = `Successfull Entered ${count} Semester Marks`
-            document.getElementById("sem-calculate-btn").removeAttribute("disabled")
+            console.log("Error")
             return
         }
     }
     else {
-        console.log("Error")
-        return
+        alert("Obtained Marks Should Less then or equal to Total Marks")
     }
+
 }
 )
 
